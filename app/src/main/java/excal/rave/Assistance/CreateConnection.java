c2d4d4f46197152b28060b7893a8110bfa1afc45
Akash <Akash>
2017-01-10 18:24:30 +0530

    commit

---
+++ app/src/main/java/excal/rave/Assistance/CreateConnection.java
@@ -0,0 +1,11 @@
+package excal.rave.Assistance;
+
+/**
+ * Created by Karan on 02-01-2017.
+ */
+
+public class CreateConnection {
+    public void sendCredentials() {
+
+    }
+}
---
+++ app/src/main/java/excal/rave/Assistance/DeviceListFragment.java
@@ -0,0 +1,13 @@
+package excal.rave.Assistance;
+
+import android.app.Fragment;
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by Karan on 04-01-2017.
+ */
+public class DeviceListFragment extends Fragment {
+    public void updateThisDevice(WifiP2pDevice thisDevice){
+
+    }
+}
